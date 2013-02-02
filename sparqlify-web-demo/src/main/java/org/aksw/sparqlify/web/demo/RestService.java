66c2c2d0d4d5a4cc3685ec07978913ecb0301101
cstadler <cstadler@informatik.uni-leipzig.de>
2013-02-02 20:30:38 +0100

    My changes to the web demo file

--- sparqlify-web-demo/src/main/java/org/aksw/sparqlify/web/demo/RestService.java
+++ sparqlify-web-demo/src/main/java/org/aksw/sparqlify/web/demo/RestService.java
@@ -1,5 +1,6 @@
 package org.aksw.sparqlify.web.demo;
 
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.HashMap;
@@ -8,30 +9,44 @@ import java.util.Map;
 import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.StreamingOutput;
 
 import org.aksw.commons.sparql.api.core.QueryExecutionFactory;
 import org.aksw.commons.util.MapReader;
+import org.aksw.commons.util.slf4j.LoggerCount;
+import org.aksw.sparqlify.config.syntax.Config;
 import org.aksw.sparqlify.core.RdfViewSystemOld;
 import org.aksw.sparqlify.core.algorithms.CandidateViewSelectorImpl;
-import org.aksw.sparqlify.core.datatypes.TypeSystem;
+import org.aksw.sparqlify.core.cast.NewWorldTest;
+import org.aksw.sparqlify.core.cast.TypeSystem;
 import org.aksw.sparqlify.core.domain.input.ViewDefinition;
 import org.aksw.sparqlify.core.interfaces.CandidateViewSelector;
 import org.aksw.sparqlify.core.interfaces.SparqlSqlRewriter;
 import org.aksw.sparqlify.core.sparql.QueryExecutionFactorySparqlifyDs;
 import org.aksw.sparqlify.util.SparqlifyUtils;
 import org.aksw.sparqlify.util.ViewDefinitionFactory;
+import org.aksw.sparqlify.web.ProcessQuery;
+import org.aksw.sparqlify.web.SparqlFormatterUtils;
+import org.aksw.sparqlify.web.StreamingOutputString;
+import org.antlr.runtime.RecognitionException;
 import org.h2.jdbcx.JdbcDataSource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.google.gson.Gson;
 import com.hp.hpl.jena.query.QueryExecution;
 import com.hp.hpl.jena.query.ResultSet;
 import com.hp.hpl.jena.query.ResultSetFormatter;
+import com.hp.hpl.jena.sparql.engine.http.HttpParams;
 
 /**
  * Jersey resource for the QA Dashboard transition backend.
@@ -43,7 +58,11 @@ import com.hp.hpl.jena.query.ResultSetFormatter;
 // @Produces("application/rdf+xml")
 // @Produces("text/plain")
 public class RestService {
+	
+	private static final Logger logger = LoggerFactory.getLogger(RestService.class);
 
+	
+	
 	/**
 	 * 
 	 * @param context
@@ -195,8 +214,8 @@ public class RestService {
 
 		// Initialisieren von Sparqlify
 		RdfViewSystemOld.initSparqlifyFunctions();
-		TypeSystem datatypeSystem = SparqlifyUtils
-				.createDefaultDatatypeSystem();
+		TypeSystem datatypeSystem = NewWorldTest.createDefaultDatatypeSystem();
+
 		// SqlTranslator sqlTranslator = new SqlTranslatorImpl(datatypeSystem);
 
 		SparqlSqlRewriter rewriter = SparqlifyUtils.createTestRewriter(
@@ -225,5 +244,189 @@ public class RestService {
 		String result = gson.toJson(map);
 
 		return result;
+	}
+	
+	
+	@GET
+	@Path("/setMapping")
+	@Produces(MediaType.APPLICATION_JSON)
+	public String setMapping(@Context HttpServletRequest req, @QueryParam("mapping") String sparqlifyMapping) throws IOException, RecognitionException 
+	{
+		HttpSession session = req.getSession(true);
+
+		LoggerCount loggerCount = new LoggerCount(logger);
+		Config config = SparqlifyUtils.createConfig(sparqlifyMapping, loggerCount);
+		
+		if(loggerCount.getErrorCount() > 0) {
+			throw new RuntimeException("Errors in the input");
+		}
+		
+		session.setAttribute("sparqlifyConfig", config);
+		
+		
+		
+		Map<String, Object> map = new HashMap<String, Object>();
+		
+		map.put("success", true);
+		
+		Gson gson = new Gson();
+		String result = gson.toJson(map);
+
+		return result;
+	}
+	
+	
+	
+	public QueryExecutionFactory buildQueryExecutionFactory(HttpServletRequest req)
+			throws SQLException, IOException
+	{
+		HttpSession session = req.getSession(true);
+
+		JdbcDataSource dataSource = (JdbcDataSource)session.getAttribute("database");
+		if(dataSource == null) {
+			throw new RuntimeException("No data source exists - probably no or errornous sql was sent");
+		}
+
+		
+		Config config = (Config)session.getAttribute("sparqlifyConfig");
+		if(config == null) {
+			throw new RuntimeException("No Sparqlify mapping exists - probably no or errornous data was sent");
+		}
+		
+		//Config config = SparqlifyUtils.readConfig(bundle.getMapping().getInputStream());
+		//DataSource ds = SparqlifyUtils.createDefaultDatabase("test", bundle.getSql().getInputStream());
+		
+		
+		QueryExecutionFactory qef = SparqlifyUtils.createDefaultSparqlifyEngine(dataSource, config, null, null);
+		
+		return qef;
+	}
+	
+	
+	
+	public StreamingOutput processQuery(HttpServletRequest req, String queryString, String format)
+			throws Exception
+	{
+		QueryExecutionFactory qef = buildQueryExecutionFactory(req);
+		
+		return ProcessQuery.processQuery(queryString, format, qef);
+	}
+
+	/*
+	@GET
+	public String executeQueryXml()
+			throws Exception {
+		String example = "<?xml version='1.0' encoding='ISO-8859-1'?><xml>Select * { ?s ?p ?o } Limit 10</xml>";
+		return "No query specified. Example: ?query=" + StringUtils.urlEncode(example);
+	}
+	*/
+
+	/*
+	@GET
+	@Produces(MediaType.TEXT_PLAIN)
+	public String executeQuery()
+			throws Exception {
+		return "No query specified";
+	}
+	*/
+	
+
+	/*
+	@GET
+	public StreamingOutput executeQueryXml(@QueryParam("query") String queryString)
+			throws Exception {
+		return processQuery(queryString, SparqlFormatterUtils.FORMAT_XML);
+	}
+	*/
+
+	
+	//@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	@GET
+	@Produces(MediaType.APPLICATION_XML)
+	public StreamingOutput executeQueryXml(@Context HttpServletRequest req, @QueryParam("query") String queryString)
+			throws Exception {
+
+		if(queryString == null) {
+			return StreamingOutputString.create("<error>No query specified. Append '?query=&lt;your SPARQL query&gt;'</error>");
+		}
+		
+		return processQuery(req, queryString, SparqlFormatterUtils.FORMAT_XML);
+	}
+
+	
+	//@Produces(MediaType.APPLICATION_XML)
+	@POST
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	public StreamingOutput executeQueryXmlPost(@Context HttpServletRequest req, @FormParam("query") String queryString)
+			throws Exception {
+
+		if(queryString == null) {
+			return StreamingOutputString.create("<error>No query specified. Append '?query=&lt;your SPARQL query&gt;'</error>");
+		}
+		
+		return processQuery(req, queryString, SparqlFormatterUtils.FORMAT_XML);
+	}
+
+	@GET
+	@Produces({MediaType.APPLICATION_JSON, "application/sparql-results+json"})
+	public StreamingOutput executeQueryJson(@Context HttpServletRequest req, @QueryParam("query") String queryString)
+			throws Exception {
+		return processQuery(req, queryString, SparqlFormatterUtils.FORMAT_Json);
+	}
+
+	@POST
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	@Produces({MediaType.APPLICATION_JSON, "application/sparql-results+json"})
+	public StreamingOutput executeQueryJsonPost(@Context HttpServletRequest req, @FormParam("query") String queryString)
+			throws Exception {
+		return processQuery(req, queryString, SparqlFormatterUtils.FORMAT_Json);
+	}
+	
+	//@Produces("application/rdf+xml")
+	//@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	@GET
+	@Produces(HttpParams.contentTypeRDFXML)
+	public StreamingOutput executeQueryRdfXml(@Context HttpServletRequest req, @QueryParam("query") String queryString)
+			throws Exception {
+		return processQuery(req, queryString, SparqlFormatterUtils.FORMAT_RdfXml);
+	}	
+	
+
+	@POST
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	@Produces(HttpParams.contentTypeRDFXML)
+	public StreamingOutput executeQueryRdfXmlPost(@Context HttpServletRequest req, @FormParam("query") String queryString)
+			throws Exception {
+		return processQuery(req, queryString, SparqlFormatterUtils.FORMAT_RdfXml);
+	}
+
+	@GET
+	@Produces("application/sparql-results+xml")
+	public StreamingOutput executeQueryResultSetXml(@Context HttpServletRequest req, @QueryParam("query") String queryString)
+			throws Exception {
+		return processQuery(req, queryString, SparqlFormatterUtils.FORMAT_XML);
+	}
+
+	@POST
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	@Produces("application/sparql-results+xml")
+	public StreamingOutput executeQueryResultSetXmlPost(@Context HttpServletRequest req, @FormParam("query") String queryString)
+			throws Exception {
+		return processQuery(req, queryString, SparqlFormatterUtils.FORMAT_XML);
+	}	
+	
+	@GET
+	@Produces(MediaType.TEXT_PLAIN)
+	public StreamingOutput executeQueryText(@Context HttpServletRequest req, @QueryParam("query") String queryString)
+			throws Exception {
+		return processQuery(req, queryString, SparqlFormatterUtils.FORMAT_Text);
+	}
+
+	@POST
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	@Produces(MediaType.TEXT_PLAIN)
+	public StreamingOutput executeQueryTextPost(@Context HttpServletRequest req, @FormParam("query") String queryString)
+			throws Exception {
+		return processQuery(req, queryString, SparqlFormatterUtils.FORMAT_Text);
 	}
 }
