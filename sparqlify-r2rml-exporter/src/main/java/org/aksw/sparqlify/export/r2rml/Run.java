6d0e1b5084e5518c915cb21203eb4beebd1e6f59
Patrick Westphal <patrick.westphal@inqbus.de>
2013-07-18 21:30:22 +0200

    R2RML export rewrite
    * now supports multiple predicate object maps in one Triples Map
    * now supports referencing object maps
    * more information (already given in SML) extracted to the R2RML RDF:
      * datatype
      * term type
      * language tag

--- sparqlify-r2rml-exporter/src/main/java/org/aksw/sparqlify/export/r2rml/Run.java
+++ sparqlify-r2rml-exporter/src/main/java/org/aksw/sparqlify/export/r2rml/Run.java
@@ -1,109 +1,122 @@
 package org.aksw.sparqlify.export.r2rml;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
-import java.sql.Connection;
+import java.io.InputStream;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.List;
 import java.util.Map;
 
-import javax.sql.DataSource;
-
 import org.aksw.commons.util.MapReader;
-import org.aksw.sparqlify.core.RdfViewSystemOld;
-import org.aksw.sparqlify.core.datatypes.TypeSystem;
+import org.aksw.sparqlify.config.lang.ConfigParser;
+import org.aksw.sparqlify.config.syntax.Config;
+import org.aksw.sparqlify.config.v0_2.bridge.SchemaProvider;
+import org.aksw.sparqlify.config.v0_2.bridge.SchemaProviderDummy;
+import org.aksw.sparqlify.config.v0_2.bridge.SyntaxBridge;
+import org.aksw.sparqlify.core.cast.TypeSystem;
 import org.aksw.sparqlify.core.domain.input.ViewDefinition;
 import org.aksw.sparqlify.util.SparqlifyUtils;
 import org.aksw.sparqlify.util.ViewDefinitionFactory;
+import org.aksw.sparqlify.validation.LoggerCount;
+import org.antlr.runtime.RecognitionException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.hp.hpl.jena.rdf.model.Model;
 
 public class Run {
 	
-	public static void main(String[] args) throws IOException, SQLException {
-		// init things
-		RdfViewSystemOld.initSparqlifyFunctions();
+	private static final Logger logger = LoggerFactory.getLogger("R2RMLExporter");
+	
+	
+	public static void main(String[] args) throws IOException, SQLException,
+			ResultNumberMismatchError, RecognitionException {
 		
-		// get dummy data source and dummy connection
-		DataSource dataSource = SparqlifyUtils.createTestDatabase();
-		Connection conn = dataSource.getConnection();
-		
-		/* get type mappings; looks like this:
-		 * #h2-type-name   aligned-type-name
-		 * bool    boolean
-		 * int2    integer
-		 * int4    integer
-		 * ...
-		 * 
-		 * and so on
-		 */
+		LoggerCount loggerCount = new LoggerCount(logger);
 		Map<String, String> typeAlias = MapReader.read(
 				new File("src/main/resources/type-map.h2.tsv"));
+		/*
+		 * In case you want to define the view definitions in place (as a
+		 * string), uncomment the following lines
+		 */
 		
-		// creates views...
-		ViewDefinitionFactory vdf =
-				SparqlifyUtils.createViewDefinitionFactory(conn, typeAlias);
+//		ViewDefinitionFactory vdf =
+//				SparqlifyUtils.createDummyViewDefinitionFactory(typeAlias);
+//		
+//		ViewDefinition personView =
+//				vdf.create(
+//					"Prefix ex:<http://ex.org/> " +
+//					"Prefix xsd:<http://www.w3.org/2001/XMLSchema#> " +
+//					"Create View person As " +
+//						"Construct {" +
+//							"?s a ex:Person." +
+//							"?u ex:worksFor ?s. " +
+//							"?s ex:name \"Hugo\"@en. " +
+//							"?s ex:blank []. " +
+//						"} " +
+//						"With " +
+//							"?s = uri(concat('http://ex.org/person/', ?id) " +
+//							"?t = typedLiteral(?name, xsd:string) " +
+//							"?u = uri(concat(ex:emp, ?id)) " +
+//						"From [[SELECT * FROM person]]"
+//				);
+//		
+//		Collection<ViewDefinition> viewDefs = Arrays.asList(personView);
+
 		
-		// example view
+		/*
+		 * Alternatively an SML view definition file can be specified by file
+		 * path as shown below 
+		 */
+		String filePath = "/tmp/sparqlify/views.sparqlify";
 		
-		ViewDefinition personView =
-				vdf.create(
-					"Prefix ex:<http://ex.org/> " +
-					"Create View person As " +
-						"Construct {" +
-							"?s a ex:Person." +
-							"?s ex:name ?t." +
-						"} " +
-						"With " +
-							"?s = uri(concat('http://ex.org/person/', ?id) " +
-							//"?s = plainLiteral(?id) " +
-							//"?t = plainLiteral(?name, 'en')" +
-							"?t = typedLiteral(?name, xsd:string)" +
-						"From [[SELECT * FROM person]]"
-				);
+		File configFile = new File(filePath);
 		
-		// another example view
-		ViewDefinition deptView =
-				vdf.create(
-					"Prefix ex:<http://ex.org/> " +
-					"Create View dept As " +
-						"Construct {" +
-							"?s a ex:Department." +
-							"?s ex:name ?t." +
-						"} " +
-						"With " +
-							"?s = uri(concat('http://ex.org/dept/', ?id) " +
-							"?t = bNode(?id) " +
-							//"?t = plainLiteral(concat('http:/foo.org/', 'OOH!', ?age), 'en')" +
-						"From [[" +
-							"SELECT id, name " +
-							"FROM dept ]]" // +
-							//"WHERE id > 23]]"
-				);
+		if (!configFile.exists()) {
+			loggerCount.error("File does not exist: " + filePath);
+
+		}
+
 		
-		// and yet another one
-		ViewDefinition personToDeptView =
-				vdf.create(
-					"Prefix ex:<http://ex.org/> " +
-					"Create View person_to_dept As " +
-						"Construct {" +
-							"?p ex:worksIn ?d" +
-						"} " +
-						"With " +
-							"?p = uri(concat('http://ex.org/person/', ?pers_id) " +
-							"?d = uri(concat('http://ex.org/dept/', ?dept_id) " +
-						"From [[SELECT * FROM person_to_dept " +
-						"JOIN dept ON person_to_dept.dept_id=dept.id]]"
-				);
+		ConfigParser parser = new ConfigParser();
+
+		InputStream in = new FileInputStream(configFile);
+		Config config;
+		try {
+			config = parser.parse(in, loggerCount);
+		} finally {
+			in.close();
+		}
 		
-		Collection<ViewDefinition> viewDefs = 
-				//Arrays.asList(deptView);
-				Arrays.asList(personView, deptView, personToDeptView);
+		List<org.aksw.sparqlify.config.syntax.ViewDefinition> syntaxViewDefs =
+				config.getViewDefinitions();
+		
+		// there is no need to have a real typeSystem, since a dummy schema
+		// provider is used later
+		TypeSystem typeSystem = null;
+		SchemaProvider schemaProvider =
+				new SchemaProviderDummy(typeSystem, typeAlias);
+		
+		
+		// since all we got until now are view definitions in a wrong format,
+		// we have to convert them using a so called syntax bridge
+		SyntaxBridge synBridge = new SyntaxBridge(schemaProvider);
+		// the target collection holding the view definitions in the right
+		//format
+		Collection<ViewDefinition> viewDefs = new ArrayList<ViewDefinition>();
+		// conversion loop
+		for (org.aksw.sparqlify.config.syntax.ViewDefinition viewDef : syntaxViewDefs) {
+			viewDefs.add(synBridge.create(viewDef));
+		}
 		
 		R2RMLExporter exporter = new R2RMLExporter(viewDefs);
 		Model r2rml = exporter.export();
 		
+		// print the result to stdout
 		r2rml.write(System.out, "TURTLE", "<http://foo.org/resources>");
 	}
 
