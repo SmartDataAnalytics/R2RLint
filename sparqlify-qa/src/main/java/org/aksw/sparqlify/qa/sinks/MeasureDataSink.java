6d3e3d60deb4419824218333bcd647b39b3f9beb
Patrick Westphal <patrick.westphal@inqbus.de>
2013-08-12 13:39:07 +0200

    minor changes on MeasureDataSink interface

--- sparqlify-qa/src/main/java/org/aksw/sparqlify/qa/sinks/MeasureDataSink.java
+++ sparqlify-qa/src/main/java/org/aksw/sparqlify/qa/sinks/MeasureDataSink.java
@@ -5,7 +5,7 @@ import org.aksw.sparqlify.qa.exceptions.NotImplementedException;
 
 public interface MeasureDataSink {
 
-	public void initMeasure(String name, String type, String parentDimension);
+	public void initMeasure(String name, String className, String parentDimension);
 	
 	public void write(MeasureDatum datum) throws NotImplementedException;
 
