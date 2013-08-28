b76e343b0d14dec6be71fff04688edfc41d97ade
Patrick Westphal <patrick.westphal@inqbus.de>
2013-08-28 17:05:49 +0200

    minor changes in SparqlifyDataset class

--- sparqlify-qa/src/main/java/org/aksw/sparqlify/qa/dataset/SparqlifyDataset.java
+++ sparqlify-qa/src/main/java/org/aksw/sparqlify/qa/dataset/SparqlifyDataset.java
@@ -2,17 +2,14 @@ package org.aksw.sparqlify.qa.dataset;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.util.Iterator;
 
 import org.aksw.commons.jena.reader.NTripleIterator;
 
 import com.hp.hpl.jena.graph.Triple;
 import com.hp.hpl.jena.rdf.model.Model;
-import com.hp.hpl.jena.rdf.model.ModelFactory;
 import com.hp.hpl.jena.rdf.model.impl.ModelCom;
 import com.hp.hpl.jena.sparql.graph.GraphFactory;
 
@@ -64,5 +61,4 @@ public class SparqlifyDataset extends ModelCom implements Model, Iterable<Triple
 	public Iterator<Triple> iterator() {
 		return it;
 	}
-
 }
