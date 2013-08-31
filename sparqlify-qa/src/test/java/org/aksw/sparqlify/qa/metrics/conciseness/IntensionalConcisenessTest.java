7f6cd2531d60bad719c699e5c6fcba0cb5911293
Patrick Westphal <patrick.westphal@inqbus.de>
2013-08-31 17:04:21 +0200

    added another intensional conciseness metric test

--- sparqlify-qa/src/test/java/org/aksw/sparqlify/qa/metrics/conciseness/IntensionalConcisenessTest.java
+++ sparqlify-qa/src/test/java/org/aksw/sparqlify/qa/metrics/conciseness/IntensionalConcisenessTest.java
@@ -15,7 +15,6 @@ import org.aksw.sparqlify.qa.sinks.BooleanTestingSink;
 import org.aksw.sparqlify.qa.sinks.ValueTestingSink;
 import org.aksw.sparqlify.util.SparqlifyUtils;
 import org.aksw.sparqlify.util.ViewDefinitionFactory;
-import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -27,6 +26,7 @@ public class IntensionalConcisenessTest {
 	ViewDefinition viewDef02;
 	ViewDefinition viewDef03;
 	ViewDefinition viewDef04;
+	ViewDefinition viewDef05;
 
 
 	@Before
@@ -36,9 +36,6 @@ public class IntensionalConcisenessTest {
 		initViewDefinitions();
 	}
 
-	@After
-	public void tearDown() throws Exception {
-	}
 
 	@Test
 	public void test01() throws NotImplementedException {
@@ -109,6 +106,21 @@ public class IntensionalConcisenessTest {
 		metric.registerMeasureDataSink(valSink);
 		
 		metric.assessMappings(Arrays.asList(viewDef04));
+		
+		float expected = 1 / (float) 3;
+		assertEquals(expected, valSink.mappingMeasureValue(metricName), 0);
+	}
+
+
+	@Test
+	public void test06() throws NotImplementedException {
+		IntensionalConciseness metric = new IntensionalConciseness();
+		String metricName = "test06";
+		metric.setName(metricName);
+		metric.setParentDimension("parent");
+		metric.registerMeasureDataSink(valSink);
+		
+		metric.assessMappings(Arrays.asList(viewDef05));
 		
 		float expected = 1 / (float) 3;
 		assertEquals(expected, valSink.mappingMeasureValue(metricName), 0);
@@ -212,6 +224,32 @@ public class IntensionalConcisenessTest {
 				"} " +
 				"With " +
 					"?dept = uri('http://ex.org/', ?id) " +
+					"?dnme = plainLiteral(?default_name) " +
+					"?dnmf = plainLiteral(?default_name) " +
+					"?dttl = plainLiteral(?name, ?lang) " +
+				"From" +
+					" foo");
+		
+		// 2 redundant term constructor
+		viewDef05 = vdf.create(
+				"Prefix ex: <http://ex.org/> " +
+				"Prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> " +
+				"Prefix rdfs:<http://www.w3.org/2000/01/rdf-schema#> " +
+				"Prefix foaf:<http://xmlns.com/foaf/0.1/> " +
+				"Prefix xsd:<http://www.w3.org/2001/XMLSchema#> " +
+				"Prefix owl:<http://www.w3.org/2002/07/owl#> " +
+				"Prefix spy:<http://aksw.org/sparqlify/> " +
+				"Create View departments As " +
+				"Construct { " +
+					"?dept a ex:Department. " +
+					"?dept rdfs:label ?dnme. " +  // dup
+					"?dept2 ex:label ?dnme. " +  // dup
+					"?dept spy:label ?dnmf. " +  // dup
+					"?dept ex:title ?dttl." +
+				"} " +
+				"With " +
+					"?dept = uri('http://ex.org/', ?id) " +
+					"?dept2 = uri('http://ex.org/', ?id) " +
 					"?dnme = plainLiteral(?default_name) " +
 					"?dnmf = plainLiteral(?default_name) " +
 					"?dttl = plainLiteral(?name, ?lang) " +
