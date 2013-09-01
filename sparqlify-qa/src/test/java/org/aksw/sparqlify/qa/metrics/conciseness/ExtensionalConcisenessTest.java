0bf993337900e33d29bc91ae7e2906881f5c67fa
Patrick Westphal <patrick.westphal@inqbus.de>
2013-09-01 02:01:10 +0200

    minor changes: cleaned imports

--- sparqlify-qa/src/test/java/org/aksw/sparqlify/qa/metrics/conciseness/ExtensionalConcisenessTest.java
+++ sparqlify-qa/src/test/java/org/aksw/sparqlify/qa/metrics/conciseness/ExtensionalConcisenessTest.java
@@ -1,7 +1,6 @@
 package org.aksw.sparqlify.qa.metrics.conciseness;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.fail;
 
 import java.io.File;
 import java.io.IOException;
