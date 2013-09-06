10cc6b7b801c7278f8469d4e60d4bfa61b3405f3
Patrick Westphal <patrick.westphal@inqbus.de>
2013-09-06 13:45:36 +0200

    added super() call to DatasetMeasureDatum constructor

--- sparqlify-qa/src/main/java/org/aksw/sparqlify/qa/sinks/DatasetMeasureDatum.java
+++ sparqlify-qa/src/main/java/org/aksw/sparqlify/qa/sinks/DatasetMeasureDatum.java
@@ -3,6 +3,7 @@ package org.aksw.sparqlify.qa.sinks;
 public class DatasetMeasureDatum extends MeasureDatum {
 	
 	public DatasetMeasureDatum(String dimension, String metric, float value) {
+		super();
 		this.dimension = dimension;
 		this.metric = metric;
 		this.value = value;
