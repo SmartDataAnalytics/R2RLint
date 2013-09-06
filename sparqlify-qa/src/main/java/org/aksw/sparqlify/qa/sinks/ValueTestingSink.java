9fed1a0aeb31e22483629bef4b32ec1ec8f7dc89
Patrick Westphal <patrick.westphal@inqbus.de>
2013-09-06 13:38:52 +0200

    simplified ValueTestingSink
    - there were a lot of useless if statements calling different methods
      all doing the same
    - removed if statements and unified all these hash maps

--- sparqlify-qa/src/main/java/org/aksw/sparqlify/qa/sinks/ValueTestingSink.java
+++ sparqlify-qa/src/main/java/org/aksw/sparqlify/qa/sinks/ValueTestingSink.java
@@ -1,133 +1,34 @@
 package org.aksw.sparqlify.qa.sinks;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 
-import org.aksw.sparqlify.qa.exceptions.NotImplementedException;
-import org.aksw.sparqlify.qa.metrics.DatasetMetric;
-import org.aksw.sparqlify.qa.metrics.MappingMetric;
 import org.aksw.sparqlify.qa.metrics.MetricImpl;
-import org.aksw.sparqlify.qa.metrics.NodeMetric;
-import org.aksw.sparqlify.qa.metrics.TripleMetric;
 
 public class ValueTestingSink implements MeasureDataSink {
 	
-	private HashMap<String, Float> mappingWrites;
-	private HashMap<String, Float> datasetWrites;
-	private HashMap<String, Float> tripleWrites;
-	private HashMap<String, Float> nodeWrites;
+	private HashMap<String, Float> writes;
 
 
 	public ValueTestingSink() {
-		mappingWrites = new HashMap<String, Float>();
-		datasetWrites = new HashMap<String, Float>();
-		tripleWrites = new HashMap<String, Float>();
-		nodeWrites = new HashMap<String, Float>();
+		writes = new HashMap<String, Float>();
 	}
 
 
 	@Override
 	public void initMeasure(String name, Class<? extends MetricImpl> cls,
-			String parentDimension) throws NotImplementedException {
+			String parentDimension) {
 		
-		List<String> interfaceNames = new ArrayList<String>();
-		
-		for (Class<?> interfce : cls.getInterfaces()){
-			interfaceNames.add(interfce.getName());
-		}
-		
-		// if node metric
-		if (interfaceNames.contains(NodeMetric.class.getName())) {
-			nodeWrites.put(name, (float) -1);
-		
-		// if triple metric
-		} else if (interfaceNames.contains(TripleMetric.class.getName())) {
-			tripleWrites.put(name, (float) -1);
-		
-		// if dataset metric
-		} else if (interfaceNames.contains(DatasetMetric.class.getName())) {
-			datasetWrites.put(name, (float) -1);
-		
-		// if mapping metric
-		} else if (interfaceNames.contains(MappingMetric.class.getName())) {
-			mappingWrites.put(name, (float) -1);
-		
-		// else
-		} else {
-			throw new NotImplementedException();
-		}
+		writes.put(name, (float) -1);
 	}
 
 
 	@Override
-	public void write(MeasureDatum datum) throws NotImplementedException {
-		
-		// if node measure
-		if (datum instanceof NodeMeasureDatum) {
-			writeNodeMeasure((NodeMeasureDatum) datum);
-		
-		// if triple measure
-		} else if (datum instanceof TripleMeasureDatum) {
-			writeTripleMeasure((TripleMeasureDatum) datum);
-		
-		// if dataset measure
-		} else if (datum instanceof DatasetMeasureDatum){
-			writeDatasetMeasure((DatasetMeasureDatum) datum);
-		
-		// if mapping measure
-		} else if (datum instanceof MappingMeasureDatum) {
-			writeMappingMeasure(datum);
-		
-		// if mapping var measure
-		} else if (datum instanceof MappingVarMeasureDatum) {
-			writeMappingMeasure(datum);
-		
-		// if mapping quad measure
-		} else if (datum instanceof MappingQuadMeasureDatum) {
-			writeMappingMeasure(datum);
-		} else {
-			throw new NotImplementedException();
-		}
+	public void write(MeasureDatum datum) {
+		writes.put(datum.getMetric(), datum.getValue());
 	}
 
 
-	/* dataset measure methods */
-	private void writeDatasetMeasure(DatasetMeasureDatum datum) {
-		datasetWrites.put(datum.getMetric(), datum.getValue());
-	}
-
-	public float datasetMeasureValue(String metricName) {
-		return datasetWrites.get(metricName);
-	}
-
-
-	/* mapping (var/quad) measure methods */ 
-	private void writeMappingMeasure(MeasureDatum datum) {
-		mappingWrites.put(datum.getMetric(), datum.getValue());
-	}
-
-	public float mappingMeasureValue(String metricName) {
-		return mappingWrites.get(metricName);
-	}
-
-
-	/* triple measure methods */
-	private void writeTripleMeasure(TripleMeasureDatum datum) {
-		tripleWrites.put(datum.getMetric(), datum.getValue());
-	}
-
-	public float tripleMeasureValue(String metricName) {
-		return tripleWrites.get(metricName);
-	}
-
-
-	/* node measure methods */
-	private void writeNodeMeasure(NodeMeasureDatum datum) {
-		nodeWrites.put(datum.getMetric(), datum.getValue());
-	}
-
-	public float nodeMeasureWritten(String metricName) {
-		return nodeWrites.get(metricName);
+	public float writtenValue(String metricName) {
+		return writes.get(metricName);
 	}
 }
