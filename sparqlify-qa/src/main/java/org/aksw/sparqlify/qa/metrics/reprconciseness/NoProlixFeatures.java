7883e3dfb5f1e96db2fe3c1bf11306356ba783aa
Patrick Westphal <patrick.westphal@inqbus.de>
2013-09-14 14:58:05 +0200

    removed Node constructions
    - replaced Node.createURI with Nodefactory.createURI

--- sparqlify-qa/src/main/java/org/aksw/sparqlify/qa/metrics/reprconciseness/NoProlixFeatures.java
+++ sparqlify-qa/src/main/java/org/aksw/sparqlify/qa/metrics/reprconciseness/NoProlixFeatures.java
@@ -12,6 +12,7 @@ import org.aksw.sparqlify.qa.metrics.PinpointMetric;
 import org.aksw.sparqlify.qa.metrics.TripleMetric;
 
 import com.hp.hpl.jena.graph.Node;
+import com.hp.hpl.jena.graph.NodeFactory;
 import com.hp.hpl.jena.graph.Node_URI;
 import com.hp.hpl.jena.graph.Triple;
 
@@ -57,30 +58,30 @@ public class NoProlixFeatures extends PinpointMetric implements TripleMetric {
 	private String rdfs = "http://www.w3.org/2000/01/rdf-schema#";
 
 	// predicates to look for
-	private Node_URI rdf_type = (Node_URI) Node.createURI(rdf+"type");
-	private Node_URI rdf_subject = (Node_URI) Node.createURI(rdf+"subject");
-	private Node_URI rdf_predicate = (Node_URI) Node.createURI(rdf+"predicate");
-	private Node_URI rdf_object = (Node_URI) Node.createURI(rdf+"object");
+	private Node_URI rdf_type = (Node_URI) NodeFactory.createURI(rdf+"type");
+	private Node_URI rdf_subject = (Node_URI) NodeFactory.createURI(rdf+"subject");
+	private Node_URI rdf_predicate = (Node_URI) NodeFactory.createURI(rdf+"predicate");
+	private Node_URI rdf_object = (Node_URI) NodeFactory.createURI(rdf+"object");
 
-	private Node_URI rdf__1 = (Node_URI) Node.createURI(rdf+"_1");
-	private Node_URI rdfs_member = (Node_URI) Node.createURI(rdfs+"member");
+	private Node_URI rdf__1 = (Node_URI) NodeFactory.createURI(rdf+"_1");
+	private Node_URI rdfs_member = (Node_URI) NodeFactory.createURI(rdfs+"member");
 
-	private Node_URI rdf_first = (Node_URI) Node.createURI(rdf+"first");
-	private Node_URI rdf_rest = (Node_URI) Node_URI.createURI(rdf+"rest");
+	private Node_URI rdf_first = (Node_URI) NodeFactory.createURI(rdf+"first");
+	private Node_URI rdf_rest = (Node_URI) NodeFactory.createURI(rdf+"rest");
 
 	List<Node_URI> predicates = new ArrayList<Node_URI>(Arrays.asList(
 			rdf_subject, rdf_predicate, rdf_object, rdf__1, rdfs_member, rdf_first,
 			rdf_rest));
 
 	// classes to look for
-	private Node rdf_Statement = Node.createURI(rdf+"Statement");
+	private Node rdf_Statement = NodeFactory.createURI(rdf+"Statement");
 
-	private Node rdf_Alt = Node.createURI(rdf+"Alt");
-	private Node rdf_Bag = Node.createURI(rdf+"Bag");
-	private Node rdf_Seq = Node.createURI(rdf+"Seq");
-	private Node rdf_Container = Node.createURI(rdfs+"Container");
+	private Node rdf_Alt = NodeFactory.createURI(rdf+"Alt");
+	private Node rdf_Bag = NodeFactory.createURI(rdf+"Bag");
+	private Node rdf_Seq = NodeFactory.createURI(rdf+"Seq");
+	private Node rdf_Container = NodeFactory.createURI(rdfs+"Container");
 
-	private Node rdf_List = Node.createURI(rdf+"List");
+	private Node rdf_List = NodeFactory.createURI(rdf+"List");
 	
 	List<Node> classes = new ArrayList<Node>(Arrays.asList(
 			rdf_Statement, rdf_Alt, rdf_Bag, rdf_Seq, rdf_Container, rdf_List));
