251dab68ce8a33bb5f54400fda2e2b6e71c354a2
Patrick Westphal <patrick.westphal@teebeutelstoerche.de>
2013-12-03 23:40:48 +0100

    cleanups and minor fixes

--- sparqlify-qa/src/test/java/org/aksw/sparqlify/qa/metrics/completeness/SchemaCompletenessTest.java
+++ sparqlify-qa/src/test/java/org/aksw/sparqlify/qa/metrics/completeness/SchemaCompletenessTest.java
@@ -26,14 +26,11 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.AbstractTransactionalJUnit4SpringContextTests;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.transaction.TransactionConfiguration;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@TransactionConfiguration(defaultRollback = true)
 @ContextConfiguration(locations={"classpath:test_val_beans.xml"})
-public class SchemaCompletenessTest extends AbstractTransactionalJUnit4SpringContextTests{
+public class SchemaCompletenessTest {
 
 	@Autowired
 	private DataSource rdb;
