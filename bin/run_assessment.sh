e8aa6841a00589ddcdd931fac3462f3d06042caf
Patrick Westphal <patrick.westphal@teebeutelstoerche.de>
2014-05-26 09:30:06 +0200

    update of run_assessment bash script

--- bin/run_assessment.sh
+++ bin/run_assessment.sh
@@ -6,5 +6,6 @@ RESDIR="src/main/resources"
 CLSDIR="target/classes/"
 
 cp $RESDIR/metrics.properties $CLSDIR/metrics.properties
+cp $RESDIR/environment.properties $CLSDIR/environment.properties
 
 $MVNEXEC exec:java -Dexec.mainClass="$MAINCLS" -e
